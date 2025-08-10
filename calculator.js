51827f715b45086fe36e77f6ded36d7fc2bc7468
Archana <valkearchana324@gmail.com>
2025-08-10 12:30:04 +0530

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,19 @@
+let display=document.getElementById("display");
+function append(value){
+    display.value+=value;
+}
+function clearDisplay(){
+    display.value=" ";
+}
+function deleteLast(){
+    display.value=display.value.slice(0,-1);
+}
+function calcualte(){
+    try{
+        display.value=eval(display.value);
+
+    }
+    catch{
+        display.value="Error";
+    }
+}
